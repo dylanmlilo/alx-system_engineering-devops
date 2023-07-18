c74d512fb5c95401b24e2434617fa7968bf90d07
dylanmlilo <dylanmlilo12@gmail.com>
2023-07-18 03:07:19 -0700

    alx-system_engineering-devops update

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# This script accepts one argument and pass it
-# to a regular expression matching method
-# The regex should not contain square brackets
-
-puts ARGV[0].scan(/hbt*n/).join
